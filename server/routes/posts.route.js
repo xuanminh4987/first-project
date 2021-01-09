607893bfdd9a9b7063cde8f1a24f8494731c5814
xuanminh4987 <xuanminh4987@gmail.com>
2021-01-09 20:09:27 +0700

    Add jQuery-post.html file.

--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -4,16 +4,6 @@ const postsController = require("../controllers/posts.controller")
 
 router.get("/", postsController.getPosts)
 
-router.get("/:pID([1-5]{1})", postsController.getPostsByID)
-
-router.get("/:position([a-z]{2})", postsController.getPostsByPosition)
-
-router.post('/:pID([1-9]{1})/:position', postsController.addNewPost)
-
-router.put('/:pID([1-5]{1})/:position([a-z]{2})', postsController.updatePositionByID)
-
-router.put('/:position([a-z]{1,2})/:pID([1-5]{1})', postsController.updateIDByPosition)
-
-router.delete('/:pID([1-5]{1})', postsController.delPostByID)
+router.post('/', postsController.postPost)
 
 module.exports = router
